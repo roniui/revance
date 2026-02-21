47fc854eb24beefaff96b5e5a35eea802141b77f
roniui <164195422+roniui@users.noreply.github.com>
2026-02-21 10:17:33 +0530

    Replace Microg installation instructions with MicroG-RE
    Updated installation instructions for MicroG to reference MicroG-RE by Morphe.

--- build.sh
+++ build.sh
@@ -155,9 +155,7 @@ wait
 rm -rf temp/tmp.*
 if [ -z "$(ls -A1 "${BUILD_DIR}")" ]; then abort "All builds failed."; fi
 
-log "\nInstall [Microg](https://github.com/ReVanced/GmsCore/releases) for non-root YouTube and YT Music APKs"
-log "Use [zygisk-detach](https://github.com/j-hc/zygisk-detach) to detach YouTube and YT Music modules from Play Store"
-log "\n[revanced-magisk-module](https://github.com/j-hc/revanced-magisk-module)\n"
+log "\nInstall [MicroG-RE](https://github.com/MorpheApp/MicroG-RE/releases) by Morphe for non-root YouTube and YT Music APKs"
 log "$(cat "$TEMP_DIR"/*/changelog.md)"
 
 SKIPPED=$(cat "$TEMP_DIR"/skipped 2>/dev/null || :)
